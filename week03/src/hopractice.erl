e2b24f91d7e4fe87598a0be51cf39702bab2de79
Raman But-Husaim <raman_but-husaim@live.com>
2017-03-09 23:14:08 +0300

    W3: ho practice - zip

--- week03/src/hopractice.erl
+++ week03/src/hopractice.erl
@@ -10,7 +10,7 @@
 -author("Rakkatakka").
 
 %% API
--export([doubleAll/1, evens/1, product/1]).
+-export([doubleAll/1, evens/1, product/1, zip/2, zip_with/3, zip_with2/3, zip2/2]).
 
 doubleAll(X) when is_list(X) ->
     lists:map(fun (El) -> El * 2 end, X).
@@ -20,3 +20,33 @@ evens(X) when is_list(X) ->
 
 product(X) when is_list(X) ->
     lists:foldr(fun (El, Accum) -> El * Accum end, 1, X).
+
+zip(Xl, Yl) when is_list(Xl) andalso is_list(Yl) ->
+    zip(Xl, Yl, []).
+
+zip([], [], Accum) ->
+    lists:reverse(Accum);
+zip([], [_Y | _Ys], Accum) ->
+    lists:reverse(Accum);
+zip([_X | _Xs], [], Accum) ->
+    lists:reverse(Accum);
+zip([X | Xs], [Y | Ys], Accum) ->
+    zip(Xs, Ys, [{X, Y} | Accum]).
+
+zip_with(Func, Xl, Yl) when is_function(Func) andalso is_list(Xl) andalso is_list(Yl) ->
+    zip_with(Func, Xl, Yl, []).
+
+zip_with(_, [], [], Accum) ->
+    lists:reverse(Accum);
+zip_with(_, [], [_Y | _Ys], Accum) ->
+    lists:reverse(Accum);
+zip_with(_, [_X | _Xs], [], Accum) ->
+    lists:reverse(Accum);
+zip_with(Func, [X | Xs], [Y | Ys], Accum) ->
+    zip_with(Func, Xs, Ys, [Func(X, Y) | Accum]).
+
+zip_with2(Func, Xl, Yl) when is_function(Func) andalso is_list(Xl) andalso is_list(Yl) ->
+    lists:map(fun ({X, Y}) -> Func(X, Y) end, zip(Xl, Yl)).
+
+zip2(Xl, Yl) when is_list(Xl) andalso is_list(Yl) ->
+    zip_with2(fun (X, Y) -> {X, Y} end, Xl, Yl).
--- week03/test/hopractice_tests.erl
+++ week03/test/hopractice_tests.erl
@@ -10,7 +10,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -author("Rakkatakka").
 
--import(hopractice, [doubleAll/1, evens/1, product/1]).
+-import(hopractice, [doubleAll/1, evens/1, product/1, zip/2, zip_with/3, zip_with2/3, zip2/2]).
 
 doubleAll_WhenEmptyListIsPassed_ShouldReturnEmptyList_test() ->
     Expected = [],
@@ -38,3 +38,91 @@ product_WhenTheListIsNonEmpty_ShouldFindTheElementProduct_test() ->
     Data = [3, 5, 2, 4, 10],
     Expected = 1200,
     ?assertEqual(Expected, product(Data)).
+
+zip_WhenTheListsAreEmpty_ShouldReturnEmptyList_test() ->
+    Expected = [],
+    ?assertEqual(Expected, zip([], [])).
+
+zip_WhenTheFirstListIsLongerThanSecond_ShouldProcessElementsAccordingToFirstListLength_test() ->
+    L1 = [3, 4, 12, 5],
+    L2 = [8, 11, 88, 7, 9, 8],
+    Expected = [{3, 8}, {4, 11}, {12, 88}, {5, 7}],
+    ?assertEqual(Expected, zip(L1, L2)).
+
+zip_WhenTheSecondListIsLongerThanFirst_ShouldProcessElementsAccordingToSecondListLength_test() ->
+    L1 = [3, 4, 12, 5, 19, 31],
+    L2 = [8, 11, 88, 7],
+    Expected = [{3, 8}, {4, 11}, {12, 88}, {5, 7}],
+    ?assertEqual(Expected, zip(L1, L2)).
+
+zip_WhenTheListsAreOfTheSameLength_ShouldProcessBothListsCompletely_test() ->
+    L1 = [1, 14, 5, 8, 4],
+    L2 = [8, 1, 12, 3, 5],
+    Expected = [{1, 8}, {14, 1}, {5, 12}, {8, 3}, {4, 5}],
+    ?assertEqual(Expected, zip(L1, L2)).
+
+zip_with_WhenTheListsAreEmpty_ShouldReturnEmptyList_test() ->
+    Expected = [],
+    ?assertEqual(Expected, zip_with(fun (X, Y) -> X + Y end, [], [])).
+
+zip_with_WhenTheFirstListIsLongerThanSecond_ShouldProcessElementsAccordingToFirstListLength_test() ->
+    L1 = [3, 4, 12, 5],
+    L2 = [8, 11, 88, 7, 9, 8],
+    Expected = [11, 15, 100, 12],
+    ?assertEqual(Expected, zip_with(fun (X, Y) -> X + Y end, L1, L2)).
+
+zip_with_WhenTheSecondListIsLongerThanFirst_ShouldProcessElementsAccordingToSecondListLength_test() ->
+    L1 = [3, 4, 12, 5, 19, 31],
+    L2 = [8, 11, 88, 7],
+    Expected = [11, 15, 100, 12],
+    ?assertEqual(Expected, zip_with(fun (X, Y) -> X + Y end, L1, L2)).
+
+zip_with_WhenTheListsAreOfTheSameLength_ShouldProcessBothListsCompletely_test() ->
+    L1 = [1, 14, 5, 8, 4],
+    L2 = [8, 1, 12, 3, 5],
+    Expected = [9, 15, 17, 11, 9],
+    ?assertEqual(Expected, zip_with(fun (X, Y) -> X + Y end, L1, L2)).
+
+zip_with2_WhenTheListsAreEmpty_ShouldReturnEmptyList_test() ->
+    Expected = [],
+    ?assertEqual(Expected, zip_with2(fun (X, Y) -> X + Y end, [], [])).
+
+zip_with2_WhenTheFirstListIsLongerThanSecond_ShouldProcessElementsAccordingToFirstListLength_test() ->
+    L1 = [3, 4, 12, 5],
+    L2 = [8, 11, 88, 7, 9, 8],
+    Expected = [11, 15, 100, 12],
+    ?assertEqual(Expected, zip_with2(fun (X, Y) -> X + Y end, L1, L2)).
+
+zip_with2_WhenTheSecondListIsLongerThanFirst_ShouldProcessElementsAccordingToSecondListLength_test() ->
+    L1 = [3, 4, 12, 5, 19, 31],
+    L2 = [8, 11, 88, 7],
+    Expected = [11, 15, 100, 12],
+    ?assertEqual(Expected, zip_with2(fun (X, Y) -> X + Y end, L1, L2)).
+
+zip_with2_WhenTheListsAreOfTheSameLength_ShouldProcessBothListsCompletely_test() ->
+    L1 = [1, 14, 5, 8, 4],
+    L2 = [8, 1, 12, 3, 5],
+    Expected = [9, 15, 17, 11, 9],
+    ?assertEqual(Expected, zip_with2(fun (X, Y) -> X + Y end, L1, L2)).
+
+zip2_WhenTheListsAreEmpty_ShouldReturnEmptyList_test() ->
+    Expected = [],
+    ?assertEqual(Expected, zip2([], [])).
+
+zip2_WhenTheFirstListIsLongerThanSecond_ShouldProcessElementsAccordingToFirstListLength_test() ->
+    L1 = [3, 4, 12, 5],
+    L2 = [8, 11, 88, 7, 9, 8],
+    Expected = [{3, 8}, {4, 11}, {12, 88}, {5, 7}],
+    ?assertEqual(Expected, zip2(L1, L2)).
+
+zip2_WhenTheSecondListIsLongerThanFirst_ShouldProcessElementsAccordingToSecondListLength_test() ->
+    L1 = [3, 4, 12, 5, 19, 31],
+    L2 = [8, 11, 88, 7],
+    Expected = [{3, 8}, {4, 11}, {12, 88}, {5, 7}],
+    ?assertEqual(Expected, zip2(L1, L2)).
+
+zip2_WhenTheListsAreOfTheSameLength_ShouldProcessBothListsCompletely_test() ->
+    L1 = [1, 14, 5, 8, 4],
+    L2 = [8, 1, 12, 3, 5],
+    Expected = [{1, 8}, {14, 1}, {5, 12}, {8, 3}, {4, 5}],
+    ?assertEqual(Expected, zip2(L1, L2)).
